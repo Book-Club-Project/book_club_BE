9e133218051a7d960c6351b9c144a8fa84189e31
Sierra Tucker <sierra.tucker121@gmail.com>
2022-02-23 15:52:58 -0700

    Test/Feat: Add books service, facade, poro design

---
+++ app/services/book_service.rb
@@ -0,0 +1,19 @@
+class BookService
+  def self.search_books(search)
+    get_data("/books/v1/volumes?q=#{search}")[:items]
+  end
+
+  def self.book_info(book_id)
+    get_data("/books/v1/volumes/#{book_id}")
+  end
+
+  private
+    def self.conn
+      Faraday.new(url: "https://www.googleapis.com")
+    end
+
+    def self.get_data(url)
+      response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+end
