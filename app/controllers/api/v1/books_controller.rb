9f9c6b66a1afc33ba72eb906fb931fe318f27fc9
Eric Chon <echon92@gmail.com>
2022-02-23 19:51:52 -0700

    endpoint test for book search passing

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::BooksController < ApplicationController
     book_id = params[:id]
     render json: BookSerializer.new(BooksFacade.get_book(book_id))
   end
+
+  def search
+    searched_title = params[:title]
+    render json: BookSerializer.new(BooksFacade.get_searched_books(searched_title))
+
+  end
 end
