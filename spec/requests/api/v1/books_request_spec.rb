893f7314c6e381a2de1bc2a6ed1cc17dd724ed51
Eric Chon <echon92@gmail.com>
2022-02-23 20:23:42 -0700

    removed a test as endpoint is not logical

--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -34,21 +34,4 @@ RSpec.describe "books API" do
     end
   end
 
-  it "it can get 20 best selling books" do
-
-    get "/api/v1/books/search?title=pride"
-
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
-
-    books = JSON.parse(response.body, symbolize_names: true)
-    expect(books).to be_an(Hash)
-
-    books.each do |book|
-      expect(book[1][0][:attributes][:title]).to eq("Pride")
-      expect(book[1][0][:attributes][:authors]).to eq(["Ibi Zoboi"])
-      expect(book[1][0][:attributes][:genres]).to eq(["Young Adult Fiction"])
-      expect(book[1][0][:attributes][:description]).to be_a(String)
-    end
-  end
 end
