aa3235cbb337974a6354bb3d54295e6ef30af9d1
Sierra Tucker <sierra.tucker121@gmail.com>
2022-02-27 14:36:45 -0700

    Feat: Add create user endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/books/search', to: 'books#search'
       resources :books, only: [:index, :show]
-      resources :users, only: [:index, :show]
+      resources :users
     end
   end
 end
