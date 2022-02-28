976aa0663229736994592e455cdaf2ca82473b91
Sierra Tucker <sierra.tucker121@gmail.com>
2022-02-28 11:20:29 -0700

    Update: model/model_spec relationshipd/validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
 
   has_many :user_clubs
   has_many :clubs, through: :user_clubs
+  has_many :comments
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many(:user_clubs) }
     it { should have_many(:clubs).through(:user_clubs) }
+    it { should have_many(:comments) }
   end
 
   describe 'test of password validations' do
