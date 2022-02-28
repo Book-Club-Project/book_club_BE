cd682526c6755b71077d03c72604023931def171
Sierra Tucker <sierra.tucker121@gmail.com>
2022-02-27 19:29:55 -0700

    Fix: spacing

--- spec/services/quote_service_spec.rb
+++ spec/services/quote_service_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe QuoteService do
   it 'returns a random quote' do
     random = QuoteService.random_quote
+  
     expect(random).to be_a Hash
 
     expect(random[:result][:quote]).to be_a String
