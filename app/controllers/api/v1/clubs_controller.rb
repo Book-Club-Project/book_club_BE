99717b257e7205ff6ab3c887065ff2d94774ed0a
Sierra Tucker <sierra.tucker121@gmail.com>
2022-02-27 19:52:53 -0700

    Feat: Add endpoint to create a new club

--- app/controllers/api/v1/clubs_controller.rb
+++ app/controllers/api/v1/clubs_controller.rb
@@ -6,4 +6,15 @@ class Api::V1::ClubsController < ApplicationController
   def show
     render json: ClubSerializer.new(Club.find(params[:id]))
   end
+
+  def create
+    club = Club.create!(club_params)
+    render json: ClubSerializer.new(club), status: 201
+  end
+
+  private
+
+  def club_params
+    params.require(:club).permit(:name, :host_id, :book_id)
+  end
 end
