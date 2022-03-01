3ddfc1525fa7eec278bf36bde675f68ca2660397
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-01 12:08:15 -0700

    Once more

--- spec/services/quote_service_spec.rb
+++ spec/services/quote_service_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuoteService do
-  it 'returns a random quote' do
-    random_quote_response = File.read('./spec/support/stubbed_api_responses/random_quote_response.json')
-    stub_request(:get, "https://www.abbreviations.com/services/v2/quotes.php?format=json&searchtype=RANDOM&uid=#{ENV['quote_id']}&tokenid=#{ENV['quote_token']}")
-    .to_return(body: random_quote_response, status: 200)
-
+  it 'returns a random quote', :vcr do
+    
     random = QuoteService.random_quote
 
     expect(random).to be_a Hash
