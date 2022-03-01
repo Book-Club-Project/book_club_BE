d6a1d0cfa0b3ca18a51aa60e1132787417be683c
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-01 11:58:59 -0700

    Try stubs

--- spec/services/quote_service_spec.rb
+++ spec/services/quote_service_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe QuoteService do
-  it 'returns a random quote', :vcr do
+  it 'returns a random quote' do
+    random_quote_response = File.read('./spec/support/stubbed_api_responses/random_quote_response.json')
+    stub_request(:get, "https://www.abbreviations.com/services/v2/quotes.php?format=json&searchtype=RANDOM&uid=#{ENV['quote_id']}&tokenid=#{ENV['quote_token']}")
+    .to_return(body: random_quote_response, status: 200)
 
     random = QuoteService.random_quote
 
