963f39898504c77f7884d7e3736d872e1d961b18
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-11 07:05:29 -0500

    Modify user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,6 +29,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email)
+    params.require(:user).permit(:username, :email, :password_digest)
   end
 end
