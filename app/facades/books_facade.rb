5e226cc2538b57875a8d591c1b913735dc6da83d
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-11 21:03:50 -0500

    Fix get_searched_books method that prevents search crashes

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -1,11 +1,11 @@
 class BooksFacade
   def self.get_searched_books(search_term)
     books = BookService.search_books(search_term)
-    if books != nil
-    books.map do |book|
-      Book.new(book)
-      # Book.new(book[:volumeInfo])
-    end
+    unless books.nil?
+      books.map do |book|
+        Book.new(book)
+        # Book.new(book[:volumeInfo])
+      end
     end
   end
 
